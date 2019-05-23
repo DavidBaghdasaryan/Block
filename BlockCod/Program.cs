2f1b65bd7663d1d43698714c8d38995a0d928c18
David Baghdasaryan <david4443444@gmail.com>
2019-05-24 02:45:42 +0400

    Add project files.

---
+++ BlockCod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockCod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[int.Parse(Console.ReadLine())];
+            string arrf = "asdasda*&^&%%#$%^#$^&*(()IHNBFRTI&*(-**--%$&*IB^&CF#@/--+sda";
+            Random rnd = new Random();
+            for (int i = 0; i < arrf.Length; i++)
+            {
+               Console.WriteLine(rnd.Next(arr.Length));
+                Console.WriteLine( arrf[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
